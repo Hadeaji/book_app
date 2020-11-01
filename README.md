13db109217202b26407d2ce1c8b6ea31852c42e4
Hadi Aji <hadeaji@gmail.com>
2020-11-01 14:40:02 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # book_app
+
+## lab 11
+
+Number and name of feature: 1. Creating
+
+Estimate of time needed to complete:  1:00:00
+
+Start time: 1:24:00
+
+Finish time: 
+
+Actual time needed to complete: 
+
+--------------------------------------------
---
+++ server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const superAgent = require('superagent');
+const { request } = require('http');
+const { response } = require('express');
+
+require('dotenv').config();
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT;
+
+app.use(express.urlencoded({extended: true}));
+app.use('/public',express.static('public'));
+
+
+app.set('view engine', 'ejs');
+
+// app.get('/location', handelGettingPeopleData);
+// app.get('/weather', handelWeather);
+// app.get('/trails', handelTrails);
+// app.get('/movies', handelMovies);
+// app.get('/yelp',handelRust);
+// app.get('/*',handelError);
+
+app.get('/hello', hello);
+
+
+function hello(request,response){
+
+  response.render('pages/index.ejs');
+
+}
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`The App Port is ${PORT}`);
+});
